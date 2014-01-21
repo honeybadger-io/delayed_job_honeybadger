d698c3d88f9a93f72254a1b2a63c0c8e1258b286
Dan Bernier and Joel Nimety <dev+dbernier+jnimety@continuity.net>
2014-01-21 11:31:05 -0500

    Fix segfault running in Ruby 2.1.0

--- lib/delayed/plugins/honeybadger.rb
+++ lib/delayed/plugins/honeybadger.rb
@@ -1,34 +1,24 @@
 module Delayed
   module Plugins
     class Honeybadger < Plugin
-      module Notify
-        def error(job, error)
-          ::Honeybadger.notify_or_ignore(
-            :error_class   => error.class.name,
-            :error_message => "#{ error.class.name }: #{ error.message }",
-            :backtrace => error.backtrace,
-            :context       => {
-              :job_id => job.id,
-              :handler => job.handler,
-              :last_error => job.last_error,
-              :attempts => job.attempts,
-              :queue => job.queue
-            }
-          )
-          super if defined?(super)
-        end
-      end
-
       callbacks do |lifecycle|
-        lifecycle.before(:invoke_job) do |job|
-          payload = job.payload_object
-          payload = payload.object if payload.is_a? Delayed::PerformableMethod
-          payload.extend Notify
-        end
-
-        lifecycle.around(:perform) do |worker, &block|
+        lifecycle.around(:invoke_job) do |job, *args, &block|
           begin
-            block.call(worker)
+            block.call(job, *args)
+          rescue Exception => error
+            ::Honeybadger.notify_or_ignore(
+              :error_class   => error.class.name,
+              :error_message => "#{ error.class.name }: #{ error.message }",
+              :backtrace     => error.backtrace,
+              :context       => {
+                :job_id        => job.id,
+                :handler       => job.handler,
+                :last_error    => job.last_error,
+                :attempts      => job.attempts,
+                :queue         => job.queue
+              }
+            )
+            raise error
           ensure
             ::Honeybadger.context.clear!
           end
